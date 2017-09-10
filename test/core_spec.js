3de4a8aaa711eb99455c2d51271ded6ee209fa2c
AJRedDevil <ajan.shresh@gmail.com>
2017-09-10 12:48:51 +0545

    next functionality to get the pair and update state

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,7 @@
 import { List, Map } from 'immutable';
 import { expect } from 'chai';
 
-import { setEntries } from '../src/core';
+import { setEntries, next } from '../src/core';
 
 describe('application logic', () => {
 
@@ -22,6 +22,22 @@ describe('application logic', () => {
             const nextState = setEntries(state, entries);
             expect(nextState).to.equal(Map({
                 entries: List.of('Titanic', 'Shutter Island')
+            }));
+        });
+    });
+
+    describe('next', () => {
+
+        it('takes the next two entries under vote', () => {
+            const state = Map({
+                entries: List.of('Titanic', 'Shutter Island', 'Inception')
+            });
+            const nextState = next(state);
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Titanic', 'Shutter Island')
+                }),
+                entries: List.of('Inception')
             }));
         });
     });
