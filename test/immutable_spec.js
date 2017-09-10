1f27141c05dc8f8b0b6d26896613121c54b01470
AJRedDevil <ajan.shresh@gmail.com>
2017-09-10 11:34:15 +0545

    Immutable test cases for List and Map

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,6 +1,8 @@
 var { expect } = require('chai');
+var { List, Map } = require('immutable');
 
 describe('immutability()', () => {
+    // Number
     describe('a number', () => {
         function increment(currentState) {
             return currentState + 1;
@@ -12,6 +14,62 @@ describe('immutability()', () => {
 
             expect(nextState).to.equal(43);
             expect(state).to.equal(42);
+        });
+    });
+
+    // List
+    describe('A List', () => {
+        
+        function addMovie(currentState, movie) {
+            return currentState.push(movie);
+        }
+
+        it('is immutable', () => {
+            let state = List.of('Titanic', 'Shutter Island');
+            let nextState = addMovie(state, 'Inception');
+
+            expect(nextState).to.equal(List.of(
+                'Titanic',
+                'Shutter Island',
+                'Inception'
+            ));
+            expect(state).to.equal(List.of(
+                'Titanic',
+                'Shutter Island'
+            ));
+        });
+    });
+
+    // Map
+    describe('A Map', () => {
+        
+        function addMovie(currentState, movie) {
+            // return currentState.set(
+            //     'movies',
+            //     currentState.get('movies').push(movie)
+            // );
+            return currentState.update('movies', movies => movies.push(movie));
+        }
+
+        it('is immutable', () => {
+            let state = Map({
+                movies: List.of('Titanic', 'Shutter Island')
+            });
+            let nextState = addMovie(state, 'Inception');
+
+            expect(nextState).to.equal(Map({
+                movies: List.of(
+                    'Titanic',
+                    'Shutter Island',
+                    'Inception'
+                )
+            }));
+            expect(state).to.equal(Map({
+                movies: List.of(
+                    'Titanic',
+                    'Shutter Island'
+                )
+            }));
         });
     });
 });
